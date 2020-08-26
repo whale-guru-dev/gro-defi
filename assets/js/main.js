2674821c6c9bd1dbe9b5899358ff231e86deb408
GOD <masterionic@gmail.com>
2020-08-27 03:21:04 +0800

    update state

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,8 @@ var GRO = {};
                 $(this).remove();
             });
         }
-        setTimeout(getLeaderboard, 1000);
-        setTimeout(checkAccount, 500);
+        // setTimeout(getLeaderboard, 1000);
+        // setTimeout(checkAccount, 500);
     });
     $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function (e) {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -257,55 +257,69 @@ function connectAccount() {
 }
 
 $(window).on("load", function() {
-    $("#home").length && particlesJS("home", {
-        particles: {
-            number: {
-                value: 28
-            },
-            color: {
-                value: ["#0182cc", "#00befa", "#0182cc"]
-            },
-            shape: {
-                type: "circle"
-            },
-            opacity: {
-                value: 1,
-                random: !1,
-                anim: {
-                    enable: !1
-                }
-            },
-            size: {
-                value: 3,
-                random: !0,
-                anim: {
-                    enable: !1
-                }
-            },
-            line_linked: {
-                enable: !1
-            },
-            move: {
-                enable: !0,
-                speed: 2,
-                direction: "none",
-                random: !0,
-                straight: !1,
-                out_mode: "out"
-            }
-        },
-        interactivity: {
-            detect_on: "canvas",
-            events: {
-                onhover: {
-                    enable: !1
-                },
-                onclick: {
-                    enable: !1
-                },
-                resize: !0
-            }
-        },
-        retina_detect: !0
-    })
+    // $("#home").length && particlesJS("home", {
+    //     particles: {
+    //         number: {
+    //             value: 28
+    //         },
+    //         color: {
+    //             value: ["#0182cc", "#00befa", "#0182cc"]
+    //         },
+    //         shape: {
+    //             type: "circle"
+    //         },
+    //         opacity: {
+    //             value: 1,
+    //             random: !1,
+    //             anim: {
+    //                 enable: !1
+    //             }
+    //         },
+    //         size: {
+    //             value: 3,
+    //             random: !0,
+    //             anim: {
+    //                 enable: !1
+    //             }
+    //         },
+    //         line_linked: {
+    //             enable: !1
+    //         },
+    //         move: {
+    //             enable: !0,
+    //             speed: 2,
+    //             direction: "none",
+    //             random: !0,
+    //             straight: !1,
+    //             out_mode: "out"
+    //         }
+    //     },
+    //     interactivity: {
+    //         detect_on: "canvas",
+    //         events: {
+    //             onhover: {
+    //                 enable: !1
+    //             },
+    //             onclick: {
+    //                 enable: !1
+    //             },
+    //             resize: !0
+    //         }
+    //     },
+    //     retina_detect: !0
+    // })
+
+
+    $.get("https://api.coingecko.com/api/v3/coins/growth-defi", function(data, status){
+        if(status === 'success') {
+            // $("#gro-supply").html(data.market_data.total_supply);
+            $("#gro-volume").html(numberWithCommas(data.market_data.total_volume.usd) + " $");
+            $("#gro-price").html(data.market_data.current_price.usd + " $");
+        }
+    });
+
 });
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
